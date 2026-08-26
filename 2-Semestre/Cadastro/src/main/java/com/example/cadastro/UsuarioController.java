fc5a1a1eb43f77e2f85814aa4d8efad32e43581f
Kaiky Paes <171882322+kaiky-paes@users.noreply.github.com>
2026-08-26 10:12:10 -0300

    Aula 26-08

--- 2-Semestre/Cadastro/src/main/java/com/example/cadastro/UsuarioController.java
+++ 2-Semestre/Cadastro/src/main/java/com/example/cadastro/UsuarioController.java
@@ -1,7 +1,15 @@
 package com.example.cadastro;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import jakarta.validation.Valid;
 
 import java.util.List;
 import java.util.Optional;
@@ -30,7 +38,7 @@ public class UsuarioController {
     }
 
     @PostMapping
-    public Usuario cadastrarUsuario(@RequestBody Usuario usuario) {
+    public Usuario cadastrarUsuario(@Valid @RequestBody Usuario usuario) {
         return usuarioRepository.save(usuario);
     }
 
