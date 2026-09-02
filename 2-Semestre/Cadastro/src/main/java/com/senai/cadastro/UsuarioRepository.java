b80232855b27a01609cdb02dcb60a780513cb0b2
Kaiky Sousa Paes <171882322+kaiky-paes@users.noreply.github.com>
2026-09-02 08:22:06 -0300

    Delete 2-Semestre/Cadastro directory

--- 2-Semestre/Cadastro/src/main/java/com/senai/cadastro/UsuarioRepository.java
+++
@@ -1,9 +0,0 @@
-package com.senai.cadastro;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.UUID;
-
-public interface UsuarioRepository extends JpaRepository<Usuario, UUID> {
-    Usuario findByNome(String nome);
-}
